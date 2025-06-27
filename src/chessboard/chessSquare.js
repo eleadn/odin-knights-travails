860592162364e349904928c0be826c1b8f3c0652
eleadn <eleano.dn@gmail.com>
2025-06-27 22:40:00 +0200

    Add pathfinding

--- src/chessboard/chessSquare.js
+++ src/chessboard/chessSquare.js
@@ -1,32 +1,59 @@
-const { CHESS_DIM_X, CHESS_DIM_Y } = require("./chessConsts");
+const { CHESS_DIM_X, CHESS_DIM_Y, KNIGHT_MOVES } = require("./chessConsts");
 
-class ChessSquare {
-	constructor(x, y) {
-		this.x = x;
-		this.y = y;
-	}
+function getNeighbors(square, possibleMoves) {
+	const neighbors = [];
+	for (let move of possibleMoves) {
+		const moveX = square[0] + move[0];
+		const moveY = square[1] + move[1];
 
-	getNeighbors(possibleMoves) {
-		const neighbors = [];
-		for (let move of possibleMoves) {
-			const moveX = this.x + move[0];
-			const moveY = this.y + move[1];
-
-			if (
-				moveX >= 0 &&
-				moveX < CHESS_DIM_X &&
-				moveY >= 0 &&
-				moveY < CHESS_DIM_Y
-			) {
-				neighbors.push(new ChessSquare(moveX, moveY));
-			}
+		if (
+			moveX >= 0 &&
+			moveX < CHESS_DIM_X &&
+			moveY >= 0 &&
+			moveY < CHESS_DIM_Y
+		) {
+			neighbors.push([moveX, moveY]);
 		}
-		return neighbors;
 	}
 
-	toString() {
-		return `${this.x},${this.y}`;
-	}
+	return neighbors;
 }
 
-module.exports = { ChessSquare };
+function knightMovesBsf(start, end) {
+	const queue = [start];
+	const visited = new Set([start.toString()]);
+	const predecessors = new Map([[start.toString(), null]]);
+
+	while (queue.length > 0) {
+		const current = queue.shift();
+
+		if (current[0] === end[0] && current[1] === end[1]) {
+			break;
+		}
+
+		const childs = getNeighbors(current, KNIGHT_MOVES);
+		for (const c of childs) {
+			if (!visited.has(c.toString())) {
+				queue.push(c);
+				visited.add(c.toString());
+				predecessors.set(c.toString(), current);
+			}
+		}
+	}
+
+	const path = [end];
+	let current = predecessors.get(path[0].toString());
+
+	while (current !== null) {
+		path.unshift(current);
+		current = predecessors.get(current.toString());
+	}
+
+	return path;
+}
+
+function knightMoves(start, end) {
+	return knightMovesBsf(start, end);
+}
+
+module.exports = { knightMoves };
